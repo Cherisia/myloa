3d906d90dab8bd04c8455ec3301edf80b6161c69
Cherisia <yhcyoon@kakao.com>
2026-05-16 05:49:51 +0900

    unoptimized 설정 추가 (Image 태그와 prefetch URL 불일치로 인한 중복 요청 수정)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.discordapp.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
