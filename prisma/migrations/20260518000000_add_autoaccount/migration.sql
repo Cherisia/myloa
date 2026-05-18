82064ca16313301c724fcb34b6285fdac88760ac
Cherisia <yhcyoon@kakao.com>
2026-05-18 23:59:38 +0900

    컬럼 중복으로 인한 빌드 에러 수정

--- prisma/migrations/20260518000000_add_autoaccount/migration.sql
+++ prisma/migrations/20260518000000_add_autoaccount/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE "Expedition" ADD COLUMN "autoAccept" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "Expedition" ADD COLUMN IF NOT EXISTS "autoAccept" BOOLEAN NOT NULL DEFAULT false;
 ALTER TABLE "Expedition" ALTER COLUMN "maxMembers" SET DEFAULT 100;
